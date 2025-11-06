5c4e87b76c7aa085f01c56237e973e8bbc43bc7e
kita456 <g140702@gmail.com>
2025-11-06 12:03:21 +0000

    fix

--- scripts/scraper/index.ts
+++ scripts/scraper/index.ts
@@ -46,7 +46,7 @@ const scrape = async () => {
 
     const [download] = await Promise.all([
       page.waitForEvent("download", { timeout: 15000 }),
-      page.locator("text=エクスポート").first().click(),
+      page.locator("text=エクスポート").last().click(),
     ]);
 
     await download.saveAs(savePath);
