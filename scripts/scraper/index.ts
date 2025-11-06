d46e40173a3b6afb3f3880058712657b7f849840
kita456 <g140702@gmail.com>
2025-11-06 11:41:38 +0000

    fix bug

--- scripts/scraper/index.ts
+++ scripts/scraper/index.ts
@@ -42,6 +42,8 @@ const scrape = async () => {
       await page.locator("text=データのエクスポート").first().click();
     }
 
+    await page.locator("text=エクスポート").first().click();
+
     const [download] = await Promise.all([
       page.waitForEvent("download", { timeout: 15000 }),
       page.locator("text=エクスポート").first().click(),
