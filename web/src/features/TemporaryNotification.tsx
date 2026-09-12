5870708755a0566cfb4150226cedba51c319120c
kfess <g140702@gmail.com>
2026-09-12 11:38:06 +0900

    feat: link upstream PR in case studies notification

--- web/src/features/TemporaryNotification.tsx
+++ web/src/features/TemporaryNotification.tsx
@@ -1,5 +1,7 @@
 import { IconInfoCircle } from '@tabler/icons-react';
-import { Alert } from '@mantine/core';
+import { Alert, Anchor } from '@mantine/core';
+
+const CASE_STUDIES_REMOVAL_PR_URL = 'https://github.com/kubernetes/website/pull/57043';
 
 export const TemporaryNotification = () => {
   return (
@@ -11,8 +13,11 @@ export const TemporaryNotification = () => {
       icon={<IconInfoCircle size={16} />}
       mb={20}
     >
-      The case studies section has been removed from the English documentation, so it is no longer
-      tracked here. Please consider removing the localized versions as well.
+      The case studies section has been removed from the English documentation (
+      <Anchor href={CASE_STUDIES_REMOVAL_PR_URL} size="sm" target="_blank" rel="noopener">
+        kubernetes/website#57043
+      </Anchor>
+      ), so it is no longer tracked here. Please consider removing the localized versions as well.
     </Alert>
   );
 };
